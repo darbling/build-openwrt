5a2eb7b0a1575aa35ca391763af7859363e1cac0
darbling <105998927+darbling@users.noreply.github.com>
2024-06-26 15:38:26 +0800

    Update diy-part1.sh

--- config/Lienol-master/diy-part1.sh
+++ config/Lienol-master/diy-part1.sh
@@ -7,7 +7,13 @@
 #========================================================================================================================
 
 # Add a feed source
-# sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+src/gz openwrt_core https://raw.cooluc.com/ilxp/lede-kmod-x86_64/main/6.6.34-1-2f7f8838769462bdeb2f8d44b56971e0
+src/gz openwrt_base https://mirrors.cloud.tencent.com/lede/snapshots/packages/x86_64/base
+src/gz openwrt_luci https://mirrors.cloud.tencent.com/lede/releases/18.06.9/packages/x86_64/luci
+src/gz openwrt_packages https://mirrors.cloud.tencent.com/lede/snapshots/packages/x86_64/packages
+src/gz openwrt_routing https://mirrors.cloud.tencent.com/lede/snapshots/packages/x86_64/routing
+src/gz openwrt_telephony https://mirrors.cloud.tencent.com/lede/snapshots/packages/x86_64/telephony
 
 # other
 # rm -rf package/utils/{ucode,fbtest}
